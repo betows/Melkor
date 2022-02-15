43bd54ebe752e5b0cb4698ed19dc7664f2062d3a
Roberto <robertoamaral56@gmail.com>
2022-02-15 10:18:29 -0300

    front end corrections

--- dwitter/forms.py
+++ dwitter/forms.py
@@ -6,11 +6,10 @@ from django.contrib.auth.models import User
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=False)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "password1", "password2")
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
